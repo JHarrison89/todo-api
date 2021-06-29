c3c81ba10f4d63bea27a243472078de9ae0d9776
Jeremaia Harrison <jeremaia.harrison@gmail.com>
2021-06-29 21:59:08 +0100

    Running rubocop & changing rubocops default settings

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 
 module Types
   class QueryType < Types::BaseObject
@@ -8,16 +9,8 @@ module Types
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
-    end
+    field :all_todos, Types::TodoType, null: false, description: 'Returns all todos'
 
-    # app/graphql/types/query_type
-    field :all_todos, [TodoType], null: false,
-    description: "Returns all todos"
     def all_todos
       Todo.all
     end
